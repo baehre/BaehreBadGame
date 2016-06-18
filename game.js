de9453d303014960347fef5dceff42e9ae8aaa1c
Andrew Baehre <baehre@vt.edu>
2016-06-18 14:12:59 -0400

    some initial files

---
+++ game.js
@@ -0,0 +1,135 @@
+//game.js
+var canvas;
+var context;
+var keys;
+var backgroundSprites;
+var tileSize = 16;
+var backgroundTileSize = 48;
+var scale = 3;
+//keep track of mouse x and y
+//var prevMouseX;
+//var prevMouseY;
+//see if mouse has been clicked or not
+//var mouseClicked = false;
+//level data. saying which tiles to use.
+var levelData = [
+[11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+[11,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,11],
+[11,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,11],
+[11,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,11],
+[11,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,11],
+[11,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,11],
+[11,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,11],
+[11,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,11],
+[11,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,11],
+[11,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,11],
+[11,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,11],
+[11,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,11],
+[11,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,11],
+[11,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,11],
+[11,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,11],
+[11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11]];
+
+var intersectionEntities;
+
+function init(){
+  //set globals
+  canvas = document.getElementById("canvas");
+  context = canvas.getContext("2d");
+
+  backgroundSprites = new Image();
+  backgroundSprites.src = "SpriteSheets/LevelSprites/levelElementSprites48.png";
+
+  canvas.width = 900;
+  canvas.height = 504;
+
+  //sets all the event handlers
+  setEventHandlers();
+}
+
+function setEventHandlers(){
+  //checking for keys
+  window.addEventListener("keydown", keyDown, false);
+  window.addEventListener("keyup", keyUp, false);
+}
+
+function keyDown(e){
+  /**if(localPlayer){
+    keys.onKeyDown(e);
+  }*/
+}
+
+function keyUp(e){
+  /**if(localPlayer){
+    keys.onKeyUp(e);
+  }*/
+}
+
+function gameLoop(){
+  update();
+  draw();
+  //the magic by Paul Irish. Apparently much smoother rendering
+  window.requestAnimFrame(gameLoop);
+}
+
+function update(){
+
+}
+
+function draw(){
+  //wipe it
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.save();
+  //shifts the canvas based around where the player is
+  //context.translate(Math.round(canvas.width/2 - localPlayer.getX()), Math.round(canvas.height/2 - localPlayer.getY()));
+  drawBackground();
+  //drawPlayer();
+  //drawProjectiles();
+  context.restore();
+}
+
+function drawBackground(){
+  var grassSprite = {x : 0, y : 0};
+  var rockSprite = {x : 0, y : 96};
+  var flowerSprite = {x : 0, y : 48};
+  var voidSprite = {x : 48, y: 0};
+  for(var y = 0; y < levelData.length; y++){
+    for(var x = 0; x < levelData[0].length; x++){
+      var tileNum = levelData[y][x];
+      //so. draw the appropriate sprite. at an x and y coordinate * 48 since that's how
+      //many pixels we want each sprite to take up
+      if(tileNum == 0){
+        context.drawImage(backgroundSprites, grassSprite.x, grassSprite.y, backgroundTileSize, backgroundTileSize, Math.round(x*backgroundTileSize), Math.round(y*backgroundTileSize), backgroundTileSize, backgroundTileSize);
+      }
+      else if(tileNum == 11){
+        context.drawImage(backgroundSprites, rockSprite.x, rockSprite.y, backgroundTileSize, backgroundTileSize, Math.round(x*backgroundTileSize), Math.round(y*backgroundTileSize), backgroundTileSize, backgroundTileSize);
+      }
+      else if(tileNum == 2){
+        context.drawImage(backgroundSprites, flowerSprite.x, flowerSprite.y, backgroundTileSize, backgroundTileSize, Math.round(x*tileSize*scale), Math.round(y*backgroundTileSize), backgroundTileSize, backgroundTileSize);
+      }
+      else{
+        context.drawImage(backgroundSprites, voidSprite.x, voidSprite.y, backgroundTileSize, backgroundTileSize, Math.round(x*backgroundTileSize), Math.round(y*backgroundTileSize), backgroundTileSize, backgroundTileSize);
+      }
+    }
+  }
+}
+
+function drawPlayer(){
+  //localPlayer.draw(context);
+}
+
+function drawProjectiles(){
+  //get the players projectiles and add them to the overall list
+  /**projectiles = localPlayer.getProjectiles();
+  for(var i = 0; i < projectiles.length; i++){
+    var tempProjectile = projectiles[i];
+    if(tempProjectile.getToRemove()){
+      projectiles.splice(i, 1);
+      continue;
+    }
+    else{
+      tempProjectile.draw(context);
+    }
+  }
+  localPlayer.setProjectiles(projectiles);*/
+}
---
+++ keys.js
@@ -0,0 +1,82 @@
+//keys.js
+/**************************************************
+** GAME KEYBOARD CLASS
+**************************************************/
+var Keys = function(up, left, right, down) {
+	var up = up || false,
+		left = left || false,
+		right = right || false,
+		down = down || false;
+
+	var onKeyDown = function(e) {
+		var that = this,
+			c = e.keyCode;
+		switch (c) {
+			// Controls
+			case 37: // Left
+				that.left = true;
+				break;
+			case 65://a
+				that.left = true;
+				break;
+			case 38: // Up
+				that.up = true;
+				break;
+			case 87://w
+				that.up = true;
+				break;
+			case 39: // Right
+				that.right = true;
+				break;
+			case 68://d
+				that.right = true;
+				break;
+			case 40: // Down
+				that.down = true;
+				break;
+			case 83://s
+				that.down = true;
+				break;
+		};
+	};
+
+	var onKeyUp = function(e) {
+		var that = this,
+			c = e.keyCode;
+		switch (c) {
+			case 37: // Left
+				that.left = false;
+				break;
+			case 65://a
+				that.left = false;
+				break;
+			case 38: // Up
+				that.up = false;
+				break;
+			case 87://w
+				that.up = false;
+				break;
+			case 39: // Right
+				that.right = false;
+				break;
+			case 68://d
+				that.right = false;
+				break;
+			case 40: // Down
+				that.down = false;
+				break;
+			case 83://s
+				that.down = false;
+				break;
+		};
+	};
+
+	return {
+		up: up,
+		left: left,
+		right: right,
+		down: down,
+		onKeyDown: onKeyDown,
+		onKeyUp: onKeyUp
+	};
+};
