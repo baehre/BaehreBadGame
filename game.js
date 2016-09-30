396a84d707e68e14aaa34ad3636fe01255434a9c
Andrew Baehre <baehre@vt.edu>
2016-09-29 22:22:07 -0400

    a dinosaur. but the animation is SUPER janky

--- game.js
+++ game.js
@@ -64,7 +64,8 @@ function init(){
   //player.js
   localPlayer = new Player(canvas, 100, 300, levelData, enemies);
   //made a function that adds a chaser and updates the enemies for the player
-  addChaser(200, 100);
+  addBoss(300, 300);
+  //addChaser(200, 100);
   //addShielder(300, 100);
   //addShooter(400, 100);
   //addChaser(200, 600);
@@ -285,5 +286,10 @@ function addShooter(shooterX, shooterY){
 // adds a shielder to the game
 function addShielder(shielderX, shielderY) {
   enemies.push(new Shielder(shielderX, shielderY, levelData, localPlayer));
+  localPlayer.setEnemies(enemies);
+}
+
+function addBoss(bossX, bossY) {
+  enemies.push(new Boss(bossX, bossY, levelData, localPlayer));
   localPlayer.setEnemies(enemies);
 }
